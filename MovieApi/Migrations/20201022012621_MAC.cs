ba63efbaea3c4e516db28b933d67a43d8dc0a0b8
Sukhmanpreet Singh <Sukhmanpreet Singh@LAPTOP-M3ODO3CB>
2020-10-30 09:57:54 +1300

    Add project files.

---
+++ MovieApi/Migrations/20201022012621_MAC.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieApi.Migrations
+{
+    public partial class MAC : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MovieName = table.Column<string>(nullable: true),
+                    Plot = table.Column<string>(nullable: true),
+                    ReleaseYear = table.Column<int>(nullable: false),
+                    IsBlockBuster = table.Column<bool>(nullable: false),
+                    CreatedDt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
---
+++ MovieApi/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApi.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }             //Primary Key
+        public string MovieName { get; set; }           //Name of a movie
+       
+        public string Plot { get; set; }            //Some plot about movie
+        public int ReleaseYear { get; set; }
+        public bool IsBlockBuster { get; set; }             //Bool is added
+        public DateTime CreatedDt { get; set; }                // Date time is also added
+    }
+}
